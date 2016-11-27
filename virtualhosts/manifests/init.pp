08aae3639f11cf93379f8f8eb3c582b5d15cdeea
wik <wikkiia@gmail.com>
2016-11-27 22:59:47 +0200

    first commit

---
+++ virtualhosts/manifests/init.pp
@@ -0,0 +1,13 @@
+class virtualhosts {
+
+	package{'apache2':
+	ensure => 'installed',
+	}
+
+	service {'apache2':
+	ensure => 'true',
+	enable => 'true',
+	require => Package['apache2'],
+	provider => 'systemd'
+	}
+}
